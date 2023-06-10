9392c59c89f82ce4b842296cc973f2e2fb145985
Jabsolu <frefrebiz@hotmail.com>
2023-06-10 17:29:42 -0400

    new python solution

--- python-solutions/two-sum.py
+++ python-solutions/two-sum.py
@@ -12,9 +12,10 @@ def twoSum(nums, target):
 
 
 # TEST CASES
-nums_one = [1,2,3,4,4,5];
-nums_two = [3,4,5,8,2];
-nums_three = [9,4,5,7,9,4];
+nums_one = [1,2,3,4,4,5]
+nums_two = [3,4,5,8,2]
+nums_three = [9,4,5,7,9,4]
+
 print(twoSum(nums_one, 7))
 print(twoSum(nums_two, 11))
 print(twoSum(nums_three, 23))
