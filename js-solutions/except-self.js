0c51a339370d5526b477127be8d596b60882b46f
Jabsolu <frefrebiz@hotmail.com>
2023-06-14 20:17:54 -0400

    new solutions

--- js-solutions/except-self.js
+++ js-solutions/except-self.js
@@ -38,25 +38,6 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
  * @return {number[]}
  */
 
-var productExceptSelf = function(nums) {
-    const output = new Array(nums.length);
-    let j = nums.length - 1;
-    let i = 0;
-    let left = 1;
-    let right = 1;
-    
-    while (i < nums.length && j >= 0) {
-        output[i]
-    }
-
-    
-
-    return output;
-};
-
-console.log(productExceptSelf([1,2,3,4]));
-// console.log(productExceptSelf([1,3,4,2]));
-
 
 var productExceptSelf = function(nums) {
     const output = new Array(nums.length);
@@ -75,3 +56,6 @@ var productExceptSelf = function(nums) {
 
     return output;
 };
+
+console.log(productExceptSelf([1,2,3,4]));
+console.log(productExceptSelf([1,3,4,2]));
